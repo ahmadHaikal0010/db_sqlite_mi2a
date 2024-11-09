2080edf000eb5a86ea818b7c08339e47e2ec4f92
Ahmad Haikal <haikala0010@gmail.com>
2024-11-09 09:49:37 +0700

    create form connect to database

---
+++ app/src/main/java/com/haikal/db_sqlite_mi2a/helper/DBHelper.kt
@@ -0,0 +1,98 @@
+package com.haikal.db_sqlite_mi2a.helper
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
+    SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
+
+    // below is the method for creating a database by a sqlite query
+    override fun onCreate(db: SQLiteDatabase) {
+        // below is a sqlite query, where column names
+        // along with their data types is given
+        val query = ("CREATE TABLE " + TABLE_NAME + " ("
+                + ID_COL + " INTEGER PRIMARY KEY, " +
+                NAMA_LENGKAP + " TEXT," +
+                KAMPUS + " TEXT," +
+                EMAIL + " TEXT," +
+                TELEPON + " TEXT" + ")")
+
+        // we are calling sqlite
+        // method for executing our query
+        db.execSQL(query)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, p1: Int, p2: Int) {
+        // this method is to check if table already exists
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME)
+        onCreate(db)
+    }
+
+    // This method is for adding data in our database
+    fun addName(nama_lengkap : String, kampus : String, email : String, telepon : String ){
+
+        // below we are creating
+        // a content values variable
+        val values = ContentValues()
+
+        // we are inserting our values
+        // in the form of key-value pair
+        values.put(NAMA_LENGKAP, nama_lengkap)
+        values.put(KAMPUS, kampus)
+        values.put(EMAIL, email)
+        values.put(TELEPON, telepon)
+
+        // here we are creating a
+        // writable variable of
+        // our database as we want to
+        // insert value in our database
+        val db = this.writableDatabase
+
+        // all values are inserted into database
+        db.insert(TABLE_NAME, null, values)
+
+        // at last we are
+        // closing our database
+        db.close()
+    }
+
+    // below method is to get
+    // all data from our database
+    fun getName(): Cursor? {
+
+        // here we are creating a readable
+        // variable of our database
+        // as we want to read value from it
+        val db = this.readableDatabase
+
+        // below code returns a cursor to
+        // read data from the database
+        return db.rawQuery("SELECT * FROM " + TABLE_NAME, null)
+
+    }
+
+    companion object{
+        // here we have defined variables for our database
+
+        // below is variable for database name
+        private val DATABASE_NAME = "DB_PEGAWAI_UDC"
+
+        // below is the variable for database version
+        private val DATABASE_VERSION = 1
+
+        // below is the variable for table name
+        val TABLE_NAME = "tb_pegawai"
+
+        // below is the variable for id column
+        val ID_COL = "id"
+
+        // below is the variable for all column
+        val NAMA_LENGKAP = "nama_lengkap"
+        val KAMPUS = "kampus"
+        val EMAIL = "email";
+        val TELEPON = "telepon"
+    }
+}
